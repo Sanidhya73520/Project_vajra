36fb6a08ec3f2c8ec0eca245a1fe8458644bd338
sanidhya73520 <23052909@kiit.ac.in>
2026-03-10 01:49:34 +0530

    Complete-Backend

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -15,21 +15,35 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
 
     const sidebarRef = useRef(null);
 
+    // useEffect(() => {
+    //     function handleClickOutside(event) {
+    //         if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+    //             setIsSidebarOpen(false);
+    //         }
+    //     }
+    //     document.addEventListener("mousedown", handleClickOutside);
+    //     return () => document.removeEventListener("mousedown", handleClickOutside);
+    // }, [setIsSidebarOpen]);
+
     useEffect(() => {
-        function handleClickOutside(event) {
-            if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-                setIsSidebarOpen(false);
-            }
+    const handleClickOutside = (event) => {
+        if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+            setIsSidebarOpen(false);
         }
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => document.removeEventListener("mousedown", handleClickOutside);
-    }, [setIsSidebarOpen]);
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+    };
+}, []);
 
     return (
         <div ref={sidebarRef} className={`z-10 bg-white dark:bg-zinc-900 min-w-68 flex flex-col h-screen border-r border-gray-200 dark:border-zinc-800 max-sm:absolute transition-all ${isSidebarOpen ? 'left-0' : '-left-full'} `} >
             <WorkspaceDropdown />
             <hr className='border-gray-200 dark:border-zinc-800' />
-            <div className='flex-1 overflow-y-scroll no-scrollbar flex flex-col'>
+            <div className='flex-1 overflow-y-auto no-scrollbar flex flex-col'>
                 <div>
                     <div className='p-4'>
                         {menuItems.map((item) => (
@@ -38,10 +52,10 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
                                 <p className='text-sm truncate'>{item.name}</p>
                             </NavLink>
                         ))}
-                        <button className='flex w-full items-center gap-3 py-2 px-4 text-gray-800 dark:text-zinc-100 cursor-pointer rounded hover:bg-gray-50 dark:hover:bg-zinc-800/60 transition-all'>
+                        <NavLink to="/settings" className='flex w-full items-center gap-3 py-2 px-4 text-gray-800 dark:text-zinc-100 cursor-pointer rounded hover:bg-gray-50 dark:hover:bg-zinc-800/60 transition-all'>
                             <SettingsIcon size={16} />
                             <p className='text-sm truncate'>Settings</p>
-                        </button>
+                        </NavLink>
                     </div>
                     <MyTasksSidebar />
                     <ProjectSidebar />
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { prisma } from "../src/db.js";
-
+import sendEmail from "../configs/nodemailer.js";
 
 export const inngest = new Inngest({ id: "project-vajra" });
 
@@ -100,9 +100,8 @@ const syncWorkspaceUpdation = inngest.createFunction(
         image_url: data.image_url,
       },
     });
-  }
+  },
 );
-
 
 //Inngest Function to delete workspace data from database
 const syncWorkspaceDeletion = inngest.createFunction(
@@ -115,9 +114,8 @@ const syncWorkspaceDeletion = inngest.createFunction(
         id: data.id,
       },
     });
-  }
-)
-
+  },
+);
 
 //Inngest Function to save workspace member data to a database
 const syncWorkspaceMemberCreation = inngest.createFunction(
@@ -132,8 +130,106 @@ const syncWorkspaceMemberCreation = inngest.createFunction(
         role: String(data.role_name).toUpperCase(),
       },
     });
-  }
+  },
 );
 
+// Inngest function to Send email on Task Creation
+const sendTaskAssignmentEmail = inngest.createFunction(
+  { id: "send-task-assignment-email" },
+  { event: "task.assigned" },
+  async ({ event, step }) => {
+    const { taskId, origin } = event.data;
 
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, syncWorkspaceCreation, syncWorkspaceUpdation, syncWorkspaceDeletion, syncWorkspaceMemberCreation];
+    const task = await prisma.task.findUnique({
+      where: { id: taskId },
+      include: { assignee: true, project: true },
+    });
+
+    await sendEmail({
+      to: task.assignee.email,
+      subject: `New Task Assignment in ${task.project.name}`,
+      body: `
+      <div style="max-width: 600px">
+      <h2>Hi ${task.assignee.name}, </h2>
+      <p style = "font-size: 16px;">  You have been assigned a new task : </p>
+      <p style ="font-size: 18px; font-weight:bold, color: #007bff; margin: 8px 0;">${task.title} </p>
+      <div style ="border: 1px solid #ddd; padding: 12px 16px; border-radius:6px; margin-bottom: 30px;">
+      <p style = "margin:6px 0;"><strong> Description:</strong>${task.description}</p>
+      <p style = "margin:6px 0;"><strong> Due Date:</strong>${new Date(task.due_date).toLocaleDateString()}</p>
+      </div>
+
+      <a href="${origin}" style="background-color: #007bff; padding: 12px 24px; border-radius: 5px; color: #fff; font-weight:600; font-size:16px; text-decoration:none">
+      View Task
+      </a>
+      <p style=" margin-top: 20px; font-size:14px; color:#6c757d;">
+
+      Please make sure to review and complete it before the due date.
+
+      </p>
+      </div>`,
+    });
+
+    if (
+      new Date(task.due_date).toLocaleDateString() !== new Date().toDateString()
+    ) {
+      await step.sleepUntil(`wait-for-the-due-date`, new Date(task.due_date));
+      await step.run(`check-if-task-is-completed`, async () => {
+        const task = await prisma.task.findUnique({
+          where: { id: taskId },
+          include: { assignee: true, project: true },
+        });
+
+        if (!task) return;
+
+        if (task.status !== "DONE") {
+          await step.run(`send-task-reminder-mail`, async () => {
+            await sendEmail({
+              to: task.assignee.email,
+              sunject: `Reminder for ${task.project.name}`,
+              body: `
+
+                   <div style="max-width: 600px">
+      <h2>Hi ${task.assignee.name}, </h2>
+
+      <p style = "font-size: 16px;">  You have a task due in ${task.project.name}: </p>
+      <p style ="font-size: 18px; font-weight:bold, color: #007bff; margin: 8px 0;">${task.title} </p>
+
+      <div style ="border: 1px solid #ddd; padding: 12px 16px; border-radius:6px; margin-bottom: 30px;">
+
+      <p style = "margin:6px 0;"><strong> Description:</strong>${task.description}</p>
+      <p style = "margin:6px 0;"><strong> Due Date:</strong>${new Date(task.due_date).toLocaleDateString()}</p>
+      </div>
+
+      <a href="${origin}" style="background-color: #007bff; padding: 12px 24px; border-radius: 5px; color: #fff; font-weight:600; font-size:16px; text-decoration:none">
+      View Task
+      </a>
+      <p style=" margin-top: 20px; font-size:14px; color:#6c757d;">
+
+      Please make sure to review and complete it before the due date.
+
+      </p>
+      </div>
+
+
+             
+             
+             
+             `,
+            });
+          });
+        }
+      });
+    }
+  },
+);
+
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+  syncWorkspaceCreation,
+  syncWorkspaceUpdation,
+  syncWorkspaceDeletion,
+  syncWorkspaceMemberCreation,
+  sendTaskAssignmentEmail
+];
